2ad0caa335f0e66e61047cea1c4ef94bb2c4776f
da122122122G <daichi.712.houzuki@gmail.com>
2026-05-14 16:16:07 +0900

    Ex_05

--- Controllers/Ex04Controller.cs
+++ Controllers/Ex04Controller.cs
@@ -18,6 +18,27 @@ public class Ex04Controller : Controller
     [HttpGet("Calc")]
     public IActionResult Calc([FromQuery(Name = "v1")] int value1, [FromQuery(Name = "v2")] int value2)
     {
+        // 演習-05 型変換エラーの場合、エラーメッセージを出力する
+        /** 追加 **/
+        if (!ModelState.IsValid) // 型変換エラー?
+        {
+            // value1とvalue2で型変換エラー
+            if ((ModelState["v1"]?.Errors.Count ?? 0) > 0
+            && (ModelState["v2"]?.Errors.Count ?? 0) > 0)
+            {
+                return Content("value1とvalue2は整数ではありません。");
+            }
+            // value1で型変換エラー
+            if ((ModelState["v1"]?.Errors.Count ?? 0) > 0)
+            {
+                return Content("value1は整数ではありません。");
+            }
+            // value2で型変換エラー
+            if ((ModelState["v2"]?.Errors.Count ?? 0) > 0)
+            {
+                return Content("value2は整数ではありません。");
+            }
+        }
         var result = value1 + value2;
         return Content($"{value1} + {value2} = {result}");
     }
