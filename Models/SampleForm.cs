aa347f47193e19de4c122540443bcb3091e76827
da122122122G <daichi.712.houzuki@gmail.com>
2026-05-15 14:00:43 +0900

    L4-4

--- Models/SampleForm.cs
+++ Models/SampleForm.cs
@@ -2,24 +2,45 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
-namespace WebApp_Exercise.Models
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+/// <summary>
+/// リスト3-3 
+/// フォームデータを保持するViewModel
+/// </summary>
+public class SampleForm
 {
     /// <summary>
-    /// リスト3-3 
-    /// フォームデータを保持するViewModel
+    /// 氏名プロパティ
     /// </summary>
-    public class SampleForm
+    /// <value></value>
+    [Display(Name = "氏名")]
+    public string? Name { get; set; }
+    /// <summary>
+    /// 年齢プロパティ
+    /// </summary>
+    /// <value></value>
+    [Display(Name = "年齢")]
+    public int Age { get; set; }
+
+    /// <summary>
+    /// リスト4-4 HTML Formを作成するタグヘルパー
+    /// </summary>
+
+    /// <summary>
+    /// 選択された結果を格納するプロパティ
+    /// </summary>
+    [Display(Name = "都道府県")]
+    public int PrefecturesId { get; set; }
+    /// <summary>
+    /// プルダウン表示するリスト
+    /// </summary>
+    /// <value></value>
+    public List<SelectListItem> PrefecturesList { get; set; } = new List<SelectListItem>
     {
-        /// <summary>
-        /// 氏名プロパティ
-        /// </summary>
-        /// <value></value>
-        public string? Name { get; set; }
-        /// <summary>
-        /// 年齢プロパティ
-        /// </summary>
-        /// <value></value>
-        public int Age { get; set; }
-    }
+        new SelectListItem{ Text="--選択されていません--", Value="0" , Selected = true },
+        new SelectListItem{ Text= "北海道", Value= "1" },
+        new SelectListItem{ Text= "青森県", Value= "2" },
+        new SelectListItem{ Text= "岩手県", Value= "3" },
+    };
 }
