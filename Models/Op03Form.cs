0cbc88ba33e1f2d6dfb684165ffbb5748a16097a
da122122122G <daichi.712.houzuki@gmail.com>
2026-05-15 09:58:45 +0900

    Ex_07

---
+++ Models/Op03Form.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp_Exercise.Models;
+
+public class Op03Form
+{
+    public int value1 { get; set; }
+    public int value2 { get; set; }
+    public int opt { get; set; }
+}
