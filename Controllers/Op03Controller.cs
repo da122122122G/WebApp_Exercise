09113d3b7864e3d8a7b26c82c96ab799373741dc
da122122122G <daichi.712.houzuki@gmail.com>
2026-05-15 10:12:35 +0900

    Op03

--- Controllers/Op03Controller.cs
+++ Controllers/Op03Controller.cs
@@ -2,11 +2,40 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApp_Exercise.Models;
 
-namespace WebApp_Exercise.Controllers
+[Route("Option03")]
+public class Op03Controller : Controller
 {
-    public class Op03Controller
+    [HttpPost("Calc")]
+    public IActionResult Calc(Op03Form form)
     {
-        
+        if (form.opt == 1)
+        {
+            var result = form.value1 + form.value2;
+            return Content($"{form.value1} + {form.value2} = {result}");
+        }
+        else if (form.opt == 2)
+        {
+            var result = form.value1 - form.value2;
+            return Content($"{form.value1} - {form.value2} = {result}");
+        }
+        else if (form.opt == 3)
+        {
+            var result = form.value1 * form.value2;
+            return Content($"{form.value1} * {form.value2} = {result}");
+        }
+        else if (form.opt == 4)
+        {
+            var result = form.value1 / form.value2;
+            return Content($"{form.value1} / {form.value2} = {result}");
+        }
+        else if (form.opt == 5)
+        {
+            var result = form.value1 % form.value2;
+            return Content($"{form.value1} % {form.value2} = {result}");
+        }
+        return Content("不明な計算種別です。");
     }
 }
