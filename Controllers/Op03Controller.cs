ac0efda1b59b1033bfeadd9b1c56266125a3a960
da122122122G <daichi.712.houzuki@gmail.com>
2026-05-15 10:35:33 +0900

    Op03Switch

--- Controllers/Op03Controller.cs
+++ Controllers/Op03Controller.cs
@@ -11,31 +11,29 @@ public class Op03Controller : Controller
     [HttpPost("Calc")]
     public IActionResult Calc(Op03Form form)
     {
-        if (form.opt == 1)
+        switch (form.opt)
         {
-            var result = form.value1 + form.value2;
-            return Content($"{form.value1} + {form.value2} = {result}");
+            case 1:
+                var result = form.value1 + form.value2;
+                return Content($"{form.value1} + {form.value2} = {result}");
+            case 2:
+                result = form.value1 - form.value2;
+                return Content($"{form.value1} - {form.value2} = {result}");
+
+            case 3:
+                result = form.value1 * form.value2;
+                return Content($"{form.value1} * {form.value2} = {result}");
+
+            case 4:
+                result = form.value1 / form.value2;
+                return Content($"{form.value1} / {form.value2} = {result}");
+
+            case 5:
+                result = form.value1 % form.value2;
+                return Content($"{form.value1} % {form.value2} = {result}");
+
+            default:
+                return Content("不明な計算種別です。");
         }
-        else if (form.opt == 2)
-        {
-            var result = form.value1 - form.value2;
-            return Content($"{form.value1} - {form.value2} = {result}");
-        }
-        else if (form.opt == 3)
-        {
-            var result = form.value1 * form.value2;
-            return Content($"{form.value1} * {form.value2} = {result}");
-        }
-        else if (form.opt == 4)
-        {
-            var result = form.value1 / form.value2;
-            return Content($"{form.value1} / {form.value2} = {result}");
-        }
-        else if (form.opt == 5)
-        {
-            var result = form.value1 % form.value2;
-            return Content($"{form.value1} % {form.value2} = {result}");
-        }
-        return Content("不明な計算種別です。");
     }
 }
