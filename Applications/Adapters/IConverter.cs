590d524bdcb80e5b2df3aaf3d0ef29f2cccde2f1
da122122122G <daichi.712.houzuki@gmail.com>
2026-05-18 14:17:42 +0900

    Ex15_2

--- Applications/Adapters/IConverter.cs
+++ Applications/Adapters/IConverter.cs
@@ -1,9 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace WebApp_Exercise.Applications.Adapters;
+
 /// <summary>
 /// TDomainに指定されたドメインオブジェクトをTTargetに指定されたクラスに変換するインターフェイス
 /// </summary>
--- Applications/Adapters/IRestorer.cs
+++ Applications/Adapters/IRestorer.cs
@@ -8,7 +8,7 @@ namespace WebApp_Exercise.Applications.Adapters;
 public interface IRestorer<TDomain, TTarget>
 {
     /// <summary>
-    ///  他のクラスからドメインオブジェクトへの復元する
+    ///  他のクラス(ターゲット)からドメインオブジェクトへの復元する
     /// </summary>
     /// <typeparam name="TTarget">対象クラスの型</typeparam>
     /// <returns>復元したドメインオブジェクト</returns>
